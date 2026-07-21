0b4b4354a8c70309765af50935990eae2e7d1d60
rusomsumsam <sbintemostofa@gmail.com>
2026-07-22 04:36:04 +0600

    implement account locking mechanism

--- project-backend/src/controllers/secure.controller.js
+++ project-backend/src/controllers/secure.controller.js
@@ -28,16 +28,46 @@ const login = async (req, res) => {
         return res.send("Secure Login Failed ❌");
     }
 
+    if (
+        user.lockedUntil &&
+        user.lockedUntil > Date.now()
+    ) {
+        return res.send("Account Locked 🔒");
+    }
+
     const isMatch = await bcrypt.compare(
         password,
         user.password
     );
 
     if (isMatch) {
-        res.send("Secure Login Success ✅");
+        user.failedAttempts = 0;
+        user.lockedUntil = null;
+
+        await user.save();
+
+        return res.send("Secure Login Success ✅");
     } else {
-        res.send("Secure Login Failed ❌");
+        user.failedAttempts += 1;
+
+        if (user.failedAttempts >= 5) {
+            user.lockedUntil =
+                new Date(Date.now() + 5 * 60 * 1000);
+
+            await user.save();
+
+            return res.send(
+                "Account Locked for 5 minutes 🔒"
+            );
+        }
+
+        await user.save();
+
+        res.send(
+            `Login Failed ❌ (${user.failedAttempts}/5)`
+        );
     }
+
 };
 
 module.exports = {
